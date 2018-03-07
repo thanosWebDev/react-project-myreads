d3b4cd4839e3815f90869192bc915bfe09b52c60
Unknown <doumanis@gmail.com>
2018-03-07 18:19:13 +0200

    Refactor shelves & header as components

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function Header () {
+    return (
+        <div className="list-books-title">
+            <h1>MyReads</h1>
+        </div>
+    )
+}
+
+export default Header
